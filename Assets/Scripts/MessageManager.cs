aeabb8c749d8f9c24b6f075b30a7b8b522316318
duncanwycliffe <dwycliff@uci.edu>
2018-01-28 01:25:59 -0800

    Text to speech needs testing

---
+++ Assets/Scripts/MessageManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+using System;
+
+public class MessageManager : MonoBehaviour {
+
+
+	public int personality;
+
+	public string fileName;
+	public Text message;
+	public Text option1;
+	public Text option2; 
+	public Text option3;
+	private int lineNumber;
+
+	//voice recognition
+	private KeywordRecognizer recognizer;
+	private Dictionary<string, System.Action> words = new Dictionary<string, System.Action>();
+
+	public static bool messageStop;
+
+	private string[] MessageAndAnswers;  
+
+	private bool isOption3;
+
+	// Use this for initialization
+	void Start () 
+	{
+		personality = 0;
+		messageStop = false; 
+		lineNumber = 0;
+		isOption3 = false; 
+		MessageAndAnswers = System.IO.File.ReadAllLines ("Assets/TextFiles/" + fileName);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (!messageStop) 
+		{
+			if (MessageAndAnswers [lineNumber].IndexOf ("P .") != -1) 
+			{
+				
+				message.text = MessageAndAnswers [lineNumber].Substring(3);
+				lineNumber++;
+				words.Add (MessageAndAnswers [lineNumber].Substring (3), () => { incPers1 (); });
+				option1.text = MessageAndAnswers [lineNumber].Substring (3);
+				lineNumber++;
+				words.Add (MessageAndAnswers [lineNumber].Substring (3), () => { incPers2 (); });
+				option2.text = MessageAndAnswers [lineNumber].Substring (3);
+				lineNumber++;
+				words.Add (MessageAndAnswers [lineNumber].Substring (3), () => { incPers3 (); });
+				option3.text = MessageAndAnswers [lineNumber].Substring (3);
+				lineNumber++;
+			}
+		}
+	}
+
+	private void TextToAction(PhraseRecognizedEventArgs args)
+	{
+		Action action;
+		if(words.TryGetValue(args.text, out action))
+			{
+				action.Invoke();
+			}
+	}
+
+	private void incPers1()
+	{
+		personality++;
+	}
+	private void incPers2()
+	{
+		personality += 2;
+	}
+	private void incPers3()
+	{
+		personality += 3;
+	}
+						
+
+	
+
+
+}
---
+++ Assets/Scripts/NarrativeManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NarrativeManager : MonoBehaviour {
+
+	public Text dialogueBox;
+
+	public string fileName;
+	private string[] allLines;
+	private int lineNumber;
+	public static bool stopNarrative;
+
+	// Use this for initialization
+	void Start () {
+		
+		stopNarrative = false;
+		lineNumber = 0;
+		allLines = System.IO.File.ReadAllLines ("Assets/TextFiles/" + fileName);
+
+		dialogueBox.text = allLines [0];
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (!stopNarrative) 
+		{
+			if (Input.GetKeyDown ("space")) 
+			{
+				lineNumber++;
+			}
+		}
+
+		if (allLines[lineNumber].IndexOf(" ") == -1) 
+		{
+			dialogueBox.text = "INCOMING MESSAGE";
+			stopNarrative = true;
+		} 
+		else 
+		{
+			
+			dialogueBox.text = allLines [lineNumber];
+		}
+
+	}
+}
